c5a1f1612d7f63b4e3e16294f4ff0db6b14434d9
gemechuH <gemechuhunduma100@gmail.com>
2025-05-05 12:53:47 -0800

    Initial commit

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -5,12 +5,12 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5rn9rxVDnZBxbmnRh6mRNx98Gi24cpl4",
-  authDomain: "clone-ef3f0.firebaseapp.com",
-  projectId: "clone-ef3f0",
-  storageBucket: "clone-ef3f0.firebasestorage.app",
-  messagingSenderId: "165706638253",
-  appId: "1:165706638253:web:761bada57ff322e3fc7724",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

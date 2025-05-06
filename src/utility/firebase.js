859f84a0d671ca8c81b70a7ac9d7e7d61def2739
gemechuH <gemechuhunduma100@gmail.com>
2025-05-06 08:28:38 -0800

    make changed

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -4,14 +4,37 @@ import { getAuth } from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 
 
+// const firebaseConfig = {
+//   apiKey: AIzaSyA5rn9rxVDnZBxbmnRh6mRNx98Gi24cpl4,
+//   authDomain: clone-ef3f0.firebaseapp.com,
+//   projectId: clone-ef3f0,
+//   storageBucket: clone-ef3f0.firebasestorage.app,
+//   messagingSenderId: 165706638253,
+//   appId: 1:165706638253:web:761bada57ff322e3fc7724,
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
+apiKey: "AIzaSyA5rn9rxVDnZBxbmnRh6mRNx98Gi24cpl4",
+
+authDomain: "clone-ef3f0.firebaseapp.com",
+
+projectId: "clone-ef3f0",
+
+storageBucket: "clone-ef3f0.firebasestorage.app",
+
+messagingSenderId: "165706638253",
+
+appId: "1:165706638253:web:761bada57ff322e3fc7724",
+
 };
+
+// VITE_FIREBASE_API_KEY=AIzaSyA5rn9rxVDnZBxbmnRh6mRNx98Gi24cpl4
+// VITE_FIREBASE_AUTH_DOMAIN=clone-ef3f0.firebaseapp.com
+// VITE_FIREBASE_PROJECT_ID=clone-ef3f0
+// VITE_FIREBASE_STORAGE_BUCKET=clone-ef3f0.firebasestorage.app
+// VITE_FIREBASE_MESSAGING_SENDER_ID=165706638253
+// VITE_FIREBASE_APP_ID=1:165706638253:web:761bada57ff322e3fc7724
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
